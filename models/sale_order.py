8640ed187cb32c5508201c843c581dfd43a765cd
wcastrocarvajal <wcastrocarvajal@users.noreply.github.com>
2020-01-06 18:47:40 -0600

    Incluye linea de pedido cuando se modifican los pesos

--- models/sale_order.py
+++ models/sale_order.py
@@ -23,11 +23,35 @@ class sale_order(models.Model):
     description = fields.Text()
     numero_contenedor = fields.Char(string="Numero Contenedor")
     marchamo = fields.Char(string="Marchamo")
-    #mostrar_campos = fields.Boolean(string="Contenedor")
-    tipo = fields.Selection([('Regular','Regular'), ('Contenedor','Contenedor')], string="Tipo", default='Regular')
-    estado = fields.Selection([('Cargando','Cargando'), ('Enviado CON peso','Enviado CON peso'), ('Enviado SIN peso','Enviado SIN peso'), ('Cancelado','Cancelado')], string='Estado', default='Cargando')
+    contenedor = fields.Selection([('20','20 Pies'), ('40','40 pies')], string="Contenedor")
+    estado = fields.Selection([('Cargando','Cargando'), ('Cargado','Cargado'), ('Enviado CON peso','Enviado CON peso'), ('Enviado SIN peso','Enviado SIN peso'), ('Cancelado','Cancelado')], string='Estado')
     peso_ingreso_completo = fields.Integer(string="Contenedor Completo")
     peso_ingreso_cabezal = fields.Integer(string="Cabezal")
     peso_salida_cabezal = fields.Integer(string="Cabezal")
     peso_salida_completo = fields.Integer(string="Contenedor Completo")
-    #total = fields.Float(compute='_compute_total', store=True, string="Peso contenedor(kg)")
+    total = fields.Float(compute='action_calcular_peso', store=True, string="Peso contenedor(kg)")
+
+    @api.one
+    @api.depends('peso_salida_completo', 'peso_ingreso_completo', 'peso_ingreso_cabezal', 'peso_salida_cabezal')
+    def action_calcular_peso(self):
+        if self.peso_ingreso_cabezal > 0 or self.peso_ingreso_completo > 0 :
+            self.estado = 'Cargando'
+            if self.peso_salida_completo > 0 or self.peso_salida_cabezal > 0 :
+                self.total = float(self.peso_salida_completo - ((self.peso_ingreso_completo - self.peso_ingreso_cabezal) + self.peso_salida_cabezal))
+            else:
+                self.total = 0
+        else:
+            self.total = 0    
+
+        if self.total > 0:
+            contiene_linea_chatarra = False
+            for order_line in self.order_line:
+                if order_line.product_id.name == "Chatarra":
+                    order_line.product_uom_qty = self.total
+                    contiene_linea_chatarra = True
+                    self.estado = "Cargado"
+            if not contiene_linea_chatarra and self.state != 'new':
+                order_id = self.env['sale.order'].search([('name', '=', self.name)])
+                producto = self.env['product.template'].search([('name', '=', 'Chatarra')])
+                self.order_line.create({'name': str(producto.name), 'order_id': order_id.id, 'product_uom_qty': self.total, 'product_id': int(producto.id)})
+                self.estado = "Cargado"
